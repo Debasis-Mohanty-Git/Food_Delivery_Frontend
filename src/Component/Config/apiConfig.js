3b080e745299f64adc2ad87916f4bdb2e772d2e7
Debasis-Mohanty-Git <moghantydebasis976@gmail.com>
2025-08-25 11:58:59 +0530

    Day 20

--- src/Component/Config/apiConfig.js
+++ src/Component/Config/apiConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_URL="https://fooddeliverybackend-production-2a4b.up.railway.app";
+export const API_URL="https://tummy-time-food-ordering-web.vercel.app";
 // export const API_URL="http://localhost:8080";
 
 export const api=axios.create({
