2d3df95b552b6ab40ed52a4d64d2cdd310b6008e
Debasis-Mohanty-Git <moghantydebasis976@gmail.com>
2025-08-25 12:15:24 +0530

    final commit

--- src/Component/Config/apiConfig.js
+++ src/Component/Config/apiConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API_URL="https://fooddeliverybackend-production-2a4b.up.railway.app";
-// export const API_URL="http://localhost:8080";
+
 
 export const api=axios.create({
     baseURL:API_URL,
